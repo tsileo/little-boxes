5b7a20717ab6a3a9151ca58431fed161cc555389
Thomas Sileo <t@a4.io>
2018-07-01 12:12:14 +0200

    Tweak for Python 3.7

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ setup(
         "License :: OSI Approved :: ISC License (ISCL)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: Implementation :: CPython",
         "Programming Language :: Python :: Implementation :: PyPy",
     ],
