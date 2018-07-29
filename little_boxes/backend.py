2175fc57a813d3406407d8bd82d3705f18352d3e
Thomas Sileo <t@a4.io>
2018-07-29 15:49:58 +0200

    Formatting

--- little_boxes/backend.py
+++ little_boxes/backend.py
@@ -70,12 +70,6 @@ class Backend(abc.ABC):
         return activity.get_actor().id == as_actor.id
 
     @abc.abstractmethod
-    def post_to_remote_inbox(
-        self, as_actor: "ap.Person", payload_encoded: str, recp: str
-    ) -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
     def base_url(self) -> str:
         pass  # pragma: no cover
 
@@ -126,101 +120,9 @@ class Backend(abc.ABC):
         return out
 
     @abc.abstractmethod
-    def inbox_check_duplicate(self, as_actor: "ap.Person", iri: str) -> bool:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
     def activity_url(self, obj_id: str) -> str:
         pass  # pragma: no cover
 
     @abc.abstractmethod
     def note_url(self, obj_id: str) -> str:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def outbox_create(self, as_actor: "ap.Person", activity: "ap.Create") -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def outbox_delete(self, as_actor: "ap.Person", activity: "ap.Delete") -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def inbox_create(self, as_actor: "ap.Person", activity: "ap.Create") -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def inbox_delete(self, as_actor: "ap.Person", activity: "ap.Delete") -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def outbox_is_blocked(self, as_actor: "ap.Person", actor_id: str) -> bool:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def inbox_new(self, as_actor: "ap.Person", activity: "ap.BaseActivity") -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def outbox_new(self, as_actor: "ap.Person", activity: "ap.BaseActivity") -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def new_follower(self, as_actor: "ap.Person", follow: "ap.Follow") -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def new_following(self, as_actor: "ap.Person", follow: "ap.Follow") -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def undo_new_follower(self, as_actor: "ap.Person", follow: "ap.Follow") -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def undo_new_following(self, as_actor: "ap.Person", follow: "ap.Follow") -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def inbox_update(self, as_actor: "ap.Person", activity: "ap.Update") -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def outbox_update(self, as_actor: "ap.Person", activity: "ap.Update") -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def inbox_like(self, as_actor: "ap.Person", activity: "ap.Like") -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def inbox_undo_like(self, as_actor: "ap.Person", activity: "ap.Like") -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def outbox_like(self, as_actor: "ap.Person", activity: "ap.Like") -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def outbox_undo_like(self, as_actor: "ap.Person", activity: "ap.Like") -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def inbox_announce(self, as_actor: "ap.Person", activity: "ap.Announce") -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def inbox_undo_announce(
-        self, as_actor: "ap.Person", activity: "ap.Announce"
-    ) -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def outbox_announce(self, as_actor: "ap.Person", activity: "ap.Announce") -> None:
-        pass  # pragma: no cover
-
-    @abc.abstractmethod
-    def outbox_undo_announce(
-        self, as_actor: "ap.Person", activity: "ap.Announce"
-    ) -> None:
         pass  # pragma: no cover
