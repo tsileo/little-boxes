d8b04056447b51d54177c7611e7312b6147fb166
Thomas Sileo <t@a4.io>
2019-04-24 00:31:46 +0200

    Update HTTP sig tests

--- tests/test_httpsig.py
+++ tests/test_httpsig.py
@@ -21,7 +21,30 @@ def test_httpsig():
     back.FETCH_MOCK["https://lol.com#lol"] = {
         "publicKey": k.to_dict(),
         "id": "https://lol.com",
+        "type": "Person",
     }
+
+    httpretty.register_uri(httpretty.POST, "https://remote-instance.com", body="ok")
+
+    auth = httpsig.HTTPSigAuth(k)
+    resp = requests.post("https://remote-instance.com", json={"ok": 1}, auth=auth)
+
+    assert httpsig.verify_request(
+        resp.request.method,
+        resp.request.path_url,
+        resp.request.headers,
+        resp.request.body,
+    )
+
+
+@httpretty.activate
+def test_httpsig_key():
+    back = InMemBackend()
+    ap.use_backend(back)
+
+    k = Key("https://lol.com", "https://lol.com/key/lol")
+    k.new()
+    back.FETCH_MOCK["https://lol.com/key/lol"] = k.to_dict()
 
     httpretty.register_uri(httpretty.POST, "https://remote-instance.com", body="ok")
 
